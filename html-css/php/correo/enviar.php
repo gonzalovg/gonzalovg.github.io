979f4c3cea2adde2be238a5249cc69da88f6780a
GonzaloVG <gverdugo@alumnos.imf.com>
2021-01-18 17:41:32 +0100

    pagina de contacto

--- html-css/php/correo/enviar.php
+++ html-css/php/correo/enviar.php
@@ -1,33 +1,39 @@
 <?php
+    //Codificación UTF-8 para todo el fichero.
+    header('Content-Type: text/html; charset=UTF-8');
 
-//Codificación UTF-8 para todo el fichero.
-header('Content-Type: text/html; charset=UTF-8');
-// Recupero detalles del envío POST
-$nombre = $_POST['nombre'];
-$cp = $_POST['cp'];
-$edad = $_POST['edad'];
-echo $nombre;
-// crea y envía el mensaje de correo electrónico
-$para = 'gonver17@gmail.com';
-$asunto = 'Nuevos Datos personalesss';
-$mensaje = '<br> Datos Personales Recibidos desde la Web <br /><br /> ';
-$mensaje .= 'Nombre: '.$nombre.' <br /> ';
-$mensaje .= 'Dirección: '.$cp.'<br /> ';
-$mensaje .= 'Edad: '.$edad.'<br /><br />';
-//Así podríamos construir una estructura completa de página HTML si
-//queremos que llegue con más forma
-$cabeceras = 'MIME-Version: 1.0'."\r\n";
-$cabeceras .= 'Content-type: text/html; charset=UTF-8'."\r\n";
-$cabeceras .= 'From:emailtest@gonzaloverdugo.000webhostapp.com';
-if (mail($para, $asunto, $mensaje, $cabeceras)) {
-    echo '<span style="font-size:1.3em;color:white;background-
-color:green;padding:5px;">Se acaba de enviar el correo, revisa tu bandeja
+    // Recupero detalles del envío POST
 
-de entrada o la de SPAM antes de ponerte nervioso...</span>';
-// header('Location: ok-email.php');
-} else {
-    echo '<span style="font-size:1.3em;color:white;background-
-color:red;padding:5px;">Error al enviar mensaje</span>';
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $cuerpo = $_POST['mensaje'];
 
-    // header('Location: error-email.php');
-}
+    // crea y envía el mensaje de correo electrónico
+
+    $para = 'gonver17@gmail.com';
+    $asunto = 'Nuevos Datos personales';
+    $mensaje = '<br> Datos Personales Recibidos desde la Web <br /><br /> ';
+    $mensaje .= 'Nombre: '.$nombre.' <br /> ';
+    $mensaje .= 'Dirección: '.$correo.'<br /> ';
+    $mensaje .= 'Teléfono: '.$telefono.'<br /><br />';
+    $mensaje .= 'Mensaje: '.$cuerpo.'<br /><br />';
+
+    //Así podríamos construir una estructura completa de página HTML si
+    //queremos que llegue con más forma
+
+    $cabeceras = 'MIME-Version: 1.0'."\r\n";
+    $cabeceras .= 'Content-type: text/html; charset=UTF-8'."\r\n";
+    $cabeceras .= 'From:emailtest@gonzaloverdugo.000webhostapp.com';
+
+    if (mail($para, $asunto, $mensaje, $cabeceras)) {
+        // echo '<span style="font-size:1.3em;color:white;background-color:green;padding:5px;">Se acaba de enviar el correo, revisa tu bandeja de entrada o la de SPAM antes de ponerte nervioso...</span>';
+        header('Location: ok-email.html');
+    } else {
+        // echo '<span style="font-size:1.3em;color:white;background-color:red;padding:5px;">Error al enviar mensaje</span>';
+        header('Location: error-email.php');
+    }
+?>    
+    
+
+
